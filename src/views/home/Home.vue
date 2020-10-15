93e176d4751e816d38467957d68d3a00d580f5ce
Jiajing <jiajing@zhiweidata.com>
2020-10-15 08:12:10 +0800

    封装请求，新增部分navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
 export default {
-  name:"home"
-}
+  name: "home",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>>
