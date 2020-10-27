acf2b9c2718401c41a468ebd29a1e302c0585728
jiajing <jiajing@zhiweidata.com>
2020-10-27 16:52:17 +0800

    tabContro的吸顶效果及当前点击type的交互

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,19 +3,27 @@
     <nav-bar class="home_nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <tab-control
+      v-show="isTabFixed"
+      ref="tabControl_1"
+      class="tab_control"
+      :titles="['流行', '新款', '精选']"
+      @tabClick="tabClick"
+    />
+
     <scroll
-      class="content"
       ref="scroll"
+      class="content"
       @scroll="contentScroll"
       @pullingUp="loadMore"
       :probe-type="3"
       :pull-up-load="true"
     >
-      <home-swiper :banners="banners" />
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad" />
       <feature-view :features="recommends"></feature-view>
       <recommend-view></recommend-view>
       <tab-control
-        class="tab_control"
+        ref="tabControl_2"
         :titles="['流行', '新款', '精选']"
         @tabClick="tabClick"
       />
@@ -61,6 +69,8 @@ export default {
       },
       currentType: "pop",
       isShowBackTop: false,
+      tabOffsetTop: 0,
+      isTabFixed: false,
     };
   },
   computed: {
@@ -78,7 +88,7 @@ export default {
     this.getHomeGoods("sell");
   },
   mounted() {
-    // 监听item中图片加载完成
+    // 1.监听item中图片加载完成
     const refresh = debounce(this.$refs.scroll.refresh, 50);
     this.$bus.$on("itemImageLoad", () => {
       refresh();
@@ -100,16 +110,25 @@ export default {
           this.currentType = "sell";
           break;
       }
+      this.$refs.tabControl_1.currentIndex = index;
+      this.$refs.tabControl_2.currentIndex = index;
     },
     backTopClick() {
       this.$refs.scroll.scrollTo(0, 0);
     },
     contentScroll(position) {
+      // 1.判断BackTop是否显示
       this.isShowBackTop = -position.y > 1000;
+
+      // 2.决定tabControl是否吸顶（position: fixed）
+      this.isTabFixed = -position.y > this.tabOffsetTop;
     },
     loadMore() {
-      this.getHomeGoods(this.currentType)
-      this.$refs.scroll.scroll.refresh()
+      this.getHomeGoods(this.currentType);
+      this.$refs.scroll.scroll.refresh();
+    },
+    swiperImageLoad() {
+      this.tabOffsetTop = this.$refs.tabControl_2.$el.offsetTop;
     },
     /**
      * 网络请求的方法
@@ -129,7 +148,7 @@ export default {
         // console.log(res);
         this.goods[type].list.push(...res.data.list);
         this.goods[type].page += 1;
-        this.$refs.scroll.finishPullUp()
+        this.$refs.scroll.finishPullUp();
       });
     },
   },
@@ -139,20 +158,13 @@ export default {
 <style lang="less" scoped>
 #home {
   position: relative;
-  padding-top: 44px;
   height: 100vh;
   .home_nav {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 9;
     background-color: var(--color-tint);
     color: #fff;
   }
   .tab_control {
-    position: sticky;
-    top: 44px;
+    position: relative;
     z-index: 9;
   }
   .content {
@@ -161,7 +173,6 @@ export default {
     bottom: 49px;
     left: 0;
     right: 0;
-    // height: calc(100vh - 44px - 49px);
     overflow: hidden;
   }
 }
--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -2,7 +2,7 @@
   <swiper ref="swiper" v-if="banners.length">
     <swiper-item v-for="(item, index) in banners" :key="index">
       <a :href="item.link">
-        <img :src="item.image" alt="" />
+        <img :src="item.image" alt="" @load="imageLoad" />
       </a>
     </swiper-item>
   </swiper>
@@ -18,17 +18,28 @@ export default {
       default: [],
     },
   },
+  data() {
+    return {
+      isLoad: false,
+    };
+  },
   components: {
     Swiper,
     SwiperItem,
   },
   methods: {
-    stopTimer() {
-      this.$refs.swiper.stopTimer();
-    },
-    startTimer() {
-      if (this.$refs.swiper) {
-        this.$refs.swiper.startTimer();
+    // stopTimer() {
+    //   this.$refs.swiper.stopTimer();
+    // },
+    // startTimer() {
+    //   if (this.$refs.swiper) {
+    //     this.$refs.swiper.startTimer();
+    //   }
+    // },
+    imageLoad() {
+      if (!this.isLoad) {
+        this.$emit("swiperImageLoad");
+        this.isLoad = true;
       }
     },
   },
