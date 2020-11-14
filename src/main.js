1066d37613a7f3c54a203aa9005b597962f4fac0
Jiajing <jiajing@zhiweidata.com>
2020-11-14 11:20:24 +0800

    解决移动端300的延迟

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import router from "./router"
 import axios from "axios"
 import store from "./store"
+
+import FastClick from "fastclick"
+
 import toast from "components/common/toast"
 
 Vue.prototype.axios = axios
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 Vue.use(toast)
+
+// 解决移动端300ms的延迟
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
