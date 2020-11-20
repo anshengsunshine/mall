08dd33f6a0037e6c08f62fc7751021e5af004471
jiajing <jiajing@zhiweidata.com>
2020-11-20 17:32:30 +0800

    分类文件搭建

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -3,24 +3,42 @@
     <nav-bar class="nav_bar">
       <div slot="center">商品分类</div>
     </nav-bar>
+    <div class="content">
+      <tab-menu :categories="categories" @selectItem="selectItem"></tab-menu>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import Scroll from "components/common/scroll/Scroll";
+import TabControl from "components/content/tabControl/TabControl";
+
+import TabMenu from "./childComps/TabMenu";
+import TabContentCategory from "./childComps/TabContentCategory";
+import TabContentDetail from "./childComps/TabContentDetail";
 
 export default {
   name: "category",
   components: {
     NavBar,
+    Scroll,
+    TabControl,
+    TabMenu,
+    TabContentCategory,
+    TabContentDetail,
   },
 };
 </script>
 
 <style lang="less" scoped>
-.wrapper {
-  height: 150px;
-  background-color: red;
-  overflow: hidden;
+#category {
+  height: 100vh;
+
+  .nav_bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
 }
 </style>
